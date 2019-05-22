024a73ed93571ad5706b2ffc6e938b15b97dd6c8
AtilioA <atiliodadalto@hotmail.com>
2019-05-22 17:01:55 -0300

    aula6

---
+++ lab/aula6/pilha.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pilha.h"
+
+tPilha *criaPilha()
+{
+    tPilha *pilha;
+
+    pilha = (tPilha *)malloc(sizeof(tPilha));
+    pilha->topo = 0;
+
+    return pilha;
+}
+
+char *getNome(tPessoa *pessoa)
+{
+    return pessoa->nome;
+}
+
+int getIdade(tPessoa *pessoa)
+{
+    return pessoa->idade;
+}
+
+char *getEnd(tPessoa *pessoa)
+{
+    return pessoa->endereco;
+}
+
+int getTopo(tPilha *pilha)
+{
+    return pilha->topo;
+}
+
+void imprimePessoa(tPessoa *pessoa)
+{
+    printf("Nome: %s\n", getNome(pessoa));
+    printf("Idade: %i\n", getIdade(pessoa));
+    printf("Endereco: %s\n", getEnd(pessoa));
+}
+
+void imprimePilha(tPilha *pilha)
+{
+    int i;
+
+    if (pilha != NULL && getTopo(pilha) != 0)
+    {
+        for (i = 1; i < getTopo(pilha); i++)
+        {
+            imprimePessoa(pilha->stack[i]);
+        }
+    }
+}
+
+tPessoa *pop(tPilha *pilha)
+{
+    tPessoa *fodase = NULL;
+
+    if (pilha != NULL)
+    {
+        pilha->topo--;
+        return pilha->stack[pilha->topo];
+    }
+
+    return fodase;
+}
+
+void push(tPessoa *pessoa, tPilha *pilha)
+{
+    if (pilha != NULL && pessoa != NULL)
+    {
+        if (getTopo(pilha) < 10)
+        {
+            pilha->stack[getTopo(pilha)] = pessoa;
+            pilha->topo++;
+        }
+        else
+        {
+            printf("Pilha cheia\n");
+        }
+    }
+}
+
+void liberaPessoa(tPessoa *pessoa)
+{
+    free(pessoa->nome);
+    pessoa->idade = -1;
+    free(pessoa->endereco);
+}
+
+void destroiPilha(tPilha *pilha)
+{
+    int i;
+
+    for (i = 0; i > getTopo(pilha); i++)
+    {
+        liberaPessoa(pilha->stack[i]);
+        free(pilha->stack[i]);
+    }
+    free(pilha);
+}
+
+/* Inicializa um TipoItem Pessoa
+* inputs: o nome, a idade e o endereco da pessoa
+* output: um ponteiro para o tipo item criado
+* pre-condicao: nome, idade e endereco validos
+* pos-condicao: tipo item criado, com os campos nome, idade e endereco copiados
+*/
+tPessoa *inicializaPessoa(char *nome, int idade, char *endereco)
+{
+    tPessoa *renanM = (tPessoa *)malloc(sizeof(tPessoa));
+
+    renanM->nome = (char *)malloc(strlen(nome) * sizeof(char) + 1);
+    strcpy(renanM->nome, nome);
+
+    renanM->idade = idade;
+
+    renanM->endereco = (char *)malloc(strlen(endereco) * sizeof(char) + 1);
+    strcpy(renanM->endereco, endereco);
+
+    return renanM;
+}
---
+++ lab/aula6/pilha.h
@@ -0,0 +1,71 @@
+#ifndef PILHA_H_
+#define PILHA_H_
+
+typedef struct pessoa
+{
+    char *nome;
+    int idade;
+    char *endereco;
+} tPessoa;
+
+typedef struct pilha
+{
+    int topo;
+    tPessoa *stack[10];
+} tPilha;
+
+/*Inicializa a Pilha
+* inputs: nenhum
+* output: P (a pilha criada)
+* pre-condicao: nenhuma
+* pos-condicao: P est� definida e vazia
+*/
+tPilha *criaPilha();
+
+/*Insere uma pessoa no topo da pilha (se houver espaco)
+* inputs: P (uma pilha) e E (uma pessoa)
+* output: nenhum
+* pre-condicao: P nao vazia e com espaco para o elemento.
+* pos-condicao: Ao final da fun��o, a pilha tem E como o elemento
+do topo
+*/
+void push(tPessoa *pessoa, tPilha *pilha);
+
+
+/*Retira uma Pessoa do topo da Pilha (se pilha n�o vazia)
+* inputs: a Pilha
+* output: a pessoa retirada do topo da pilha
+* pre-condicao: Pilha n�o � nula e n�o � vazia
+* pos-condicao: pilha n�o cont�m o elemento retirado do topo
+*/
+tPessoa *pop(tPilha *pilha);
+
+
+/*Imprime as pessoas da pilha
+ * inputs: a pilha
+ * output: nenhum
+ * pre-condicao: pilha P n�o vazia
+ * pos-condicao: Pilha permanece inalterada
+ */
+void imprimePessoa(tPessoa *pessoa);
+void imprimePilha(tPilha *pilha);
+
+
+/*Libera a memoria ocupada pela pilha
+* inputs: a Pilha
+* output: NULL
+* pre-condicao: Pilha n�o � nula
+* pos-condicao: Toda memoria eh liberada
+*/
+void destroiPilha(tPilha *pilha);
+
+/*Inicializa um TipoItem Pessoa
+* inputs: o nome, a idade e o endereco da pessoa
+* output: um ponteiro para o tipo item criado
+* pre-condicao: nome, idade e endereco validos
+* pos-condicao: tipo item criado, com os campos nome, idade e endereco copiados
+*/
+tPessoa *inicializaPessoa(char* nome, int idade, char* endereco);
+
+
+#endif /* PILHA_H_ */
